17af8fd02a8e724c4d93d046e448bb36764b45c3
danny-boulay <danny.creating@gmail.com>
2025-10-14 17:24:08 -0400

    Exercises 2.1.-2.5.

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,29 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
+    </div>
+  )
+}
+
+const Header = (props) => <h2>{props.course}</h2>
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p style={{fontWeight: "bold"}}>total of {props.total} exercises</p>
+
+export default Course
